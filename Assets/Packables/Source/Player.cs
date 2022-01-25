eafbdf4c080106febebefb94796f88aa582a0bf3
Juan Felipe Cadavid <jcadavid@users.noreply.github.com>
2022-01-25 02:55:00 -0500

    Improve player animation and start bomb implementation

--- Assets/Packables/Source/Player.cs
+++ Assets/Packables/Source/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField]
     public float _movementSpeed = 5f;
